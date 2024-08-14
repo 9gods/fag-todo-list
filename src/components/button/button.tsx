d271398b2781756d1ba34c2d537189515de3fe1b
Comelli <codingcomelli@gmail.com>
2024-08-14 02:37:45 -0300

    refactor: button component tweaks

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,34 +1,32 @@
-import { ButtonHTMLAttributes, FC } from "react";
+import { ButtonHTMLAttributes, FC } from "react"
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string
   buttonType: 'text' | 'icon'
-  children: JSX.Element | JSX.Element[]
+  children?: JSX.Element | JSX.Element[]
 }
 
 const ButtonTypes: Record<string, string> = {
-  "text": "py-1 gap-2",
+  "text": "py-2 gap-2",
   "icon": " py-2"
 }
 
-const Button: FC<ButtonProps> = ({
+export const Button: FC<ButtonProps> = ({
   label,
   buttonType,
   children,
   ...rest
 }) => {
   return (
-    <button className={`flex px-2 ${buttonType ? ButtonTypes[buttonType] : ""} justify-around items-center rounded-md border-2 outline-none bg-indigo-600 border-indigo-500 hover:bg-indigo-500 hover:border-indigo-400 focus:border-zinc-200 active:border-indigo-600 active:bg-indigo-700 transition-colors duration-200`}
+    <button className={`flex w-fit h-fit px-2 ${buttonType ? ButtonTypes[buttonType] : ""} justify-around items-center rounded-md border-2 outline-none bg-indigo-600 border-indigo-500 hover:bg-indigo-500 hover:border-indigo-400 focus:border-zinc-200 active:border-indigo-600 active:bg-indigo-700 text-zinc-50 transition-colors duration-200`}
       {...rest}
     >
       {label && (
-        <p className="font-medium text-zinc-50">{label}</p>
+        <p className="font-medium select-none text-nowrap">{label}</p>
       )}
-      <div className=" text-zinc-50">
+      <div>
         {children}
       </div>
     </button>
   )
 }
-
-export default Button
