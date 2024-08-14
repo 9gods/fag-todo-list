bfd5d676b8cb517696227e79e77920d8ea7c0b58
Comelli <codingcomelli@gmail.com>
2024-08-14 02:38:08 -0300

    feat(component): implements display component

---
+++ src/components/display/display.tsx
@@ -0,0 +1,14 @@
+import { Anchor } from "../anchor/anchor"
+
+export const Display = () => {
+  return (
+    <div className="w-full h-auto">
+      <h1 className="text-4xl font-bold text-zinc-200">To-Do List</h1>
+      <p className=" mt-2 text-lg font-medium leading-tight text-zinc-400">This is a task list app built with React and TypeScript, using Tailwind CSS for styling. It allows users to add, complete, and remove tasks with smooth animations for a modern, fluid user experience.</p>
+      <div className="flex gap-3 mt-8">
+        <Anchor href="https://discordapp.com/users/214897420461408267" label="discord" />
+        <Anchor href="https://github.com/9gods" label="github" />
+      </div>
+    </div>
+  )
+}
