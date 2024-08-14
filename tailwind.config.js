9ca5142b68984f0d6111c1ab47173a9c046122d5
Comelli <codingcomelli@gmail.com>
2024-08-14 02:35:54 -0300

    chore: implements animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,52 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'slide-in': 'slideIn 0.3s ease-out forwards',
+        'slide-out': 'slideOut 0.3s ease-in-out forwards',
+        'fade-in': 'fadeIn 0.3s ease-out forwards',
+        'fade-out': 'fadeOut 0.3s ease-in forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '1',
+          },
+        },
+        fadeOut: {
+          '0%': {
+            opacity: '1',
+          },
+          '100%': {
+            opacity: '0',
+          }
+        },
+        slideIn: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        slideOut: {
+          '0%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateX(50px)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
