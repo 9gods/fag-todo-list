7e3ca6416265cf1029eb2fcad011d7e7fda10f80
9gods <codingcomelli@gmail.com>
2024-08-27 14:32:17 -0300

    feat(component): implements about

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,14 @@
+namespace Home {
+  export const Page = () => {
+    return (
+      <div className="flex flex-col min-w-lg h-fit p-4 gap-3">
+        <div className="text-4xl text-zinc-200">
+          <h1>What is a To-do List?</h1>
+        </div>
+        <p className="text-2xl text-zinc-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum quaerat quod atque beatae aspernatur illo vel minima numquam similique laudantium, neque debitis iure recusandae itaque sint delectus a! Placeat, nihil?</p>
+      </div>
+    )
+  }
+}
+
+export default Home.Page
